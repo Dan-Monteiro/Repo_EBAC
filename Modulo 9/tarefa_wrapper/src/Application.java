d6ec687f77ed0150554449da5d54dfe4b6beb675
Daniel Monteiro <daniel.monteirofilho@outlook.com>
2022-03-17 21:48:37 -0300

    Task modulo 9

---
+++ Modulo 9/tarefa_wrapper/src/Application.java
@@ -0,0 +1,8 @@
+public class Application {
+
+    public static void main(String[] args) {
+        double valor = Double.parseDouble(args[0]);
+        Double valorWrapper = valor;
+        System.out.println(valorWrapper);
+    }
+}
