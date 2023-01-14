c0106d3b366737a490d69c230ecbf967cc405496
Daniel Monteiro <daniel.monteirofilho@outlook.com>
2023-01-14 14:55:57 -0300

    feat(Task modulo 30): adiciona e atualiza campo
    Adiciona campo marca ao contexto de produto

--- Modulo 30/PROJETO 3 - PARTE 2/src/domain/model/Produto.java
+++ Modulo 30/PROJETO 3 - PARTE 2/src/domain/model/Produto.java
@@ -26,6 +26,9 @@ public class Produto implements Persistente {
     @ColunaTabela(dbName = "valor", setJavaName = "setValor")
     private BigDecimal valor;
 
+    @ColunaTabela(dbName = "marca", setJavaName = "setMarca")
+    private String marca;
+
     public String getCodigo() {
         return codigo;
     }
@@ -64,5 +67,13 @@ public class Produto implements Persistente {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
 }
