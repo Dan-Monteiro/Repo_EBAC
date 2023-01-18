44fd71916da36a875d54313e06afbc2606b740d3
Daniel Monteiro <daniel.monteirofilho@outlook.com>
2023-01-17 21:05:01 -0300

    feat(Task modulo 30): Agrega campos em factories

--- Modulo 30/PROJETO 3 - PARTE 2/src/domain/dao/factory/ClienteFactory.java
+++ Modulo 30/PROJETO 3 - PARTE 2/src/domain/dao/factory/ClienteFactory.java
@@ -17,6 +17,7 @@ public class ClienteFactory {
         cliente.setNumero(rs.getInt(("NUMERO")));
         cliente.setCidade(rs.getString(("CIDADE")));
         cliente.setEstado(rs.getString(("ESTADO")));
+        cliente.setDataCadastro(rs.getTimestamp("DATA_CADASTRO").toInstant());
         return cliente;
     }
 }
